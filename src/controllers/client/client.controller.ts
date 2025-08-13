7899bf432cc25bf8e018b8e0930fb3a64b92ae0c
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-08-13 19:20:28 +0200

    Include live client shares in chart endpoint

--- src/controllers/client/client.controller.ts
+++ src/controllers/client/client.controller.ts
@@ -58,6 +58,19 @@ export class ClientController {
         @Query('range') range: '1d' | '3d' | '7d' = '1d'
     ) {
         const chartData = await this.clientStatisticsService.getChartDataForAddress(address, range);
+
+        const coeff = 1000 * 60 * 10;
+        const currentSlot = Math.floor(Date.now() / coeff) * coeff;
+        const liveClients = this.stratumV1Service.getClientsByAddress(address);
+        let liveHashRate = 0;
+
+        for (const client of liveClients) {
+            if (client.statistics.currentTimeSlot === currentSlot) {
+                liveHashRate += client.statistics.hashRate;
+            }
+        }
+
+        chartData.push({ label: new Date(currentSlot).toISOString(), data: liveHashRate });
         return chartData;
     }
 
