cfd85bf88f2c9471cd50544843fee67c3ba8519a
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-11-01 23:05:25 +0100

    Make anomalous diff detection configurable

--- src/ORM/pool-rejected-statistics/pool-rejected-statistics.module.ts
+++ src/ORM/pool-rejected-statistics/pool-rejected-statistics.module.ts
@@ -1,13 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 import { PoolRejectedStatisticsEntity } from './pool-rejected-statistics.entity';
 import { PoolRejectedStatisticsService } from './pool-rejected-statistics.service';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([PoolRejectedStatisticsEntity]), ScheduleModule],
+  imports: [
+    TypeOrmModule.forFeature([PoolRejectedStatisticsEntity]),
+    ScheduleModule,
+    ConfigModule,
+  ],
   providers: [PoolRejectedStatisticsService],
   exports: [TypeOrmModule, ScheduleModule, PoolRejectedStatisticsService],
 })
